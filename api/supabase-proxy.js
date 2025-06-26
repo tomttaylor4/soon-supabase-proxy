f0b325aeb98ed438eb046b17ac35c400393029a7
tomttaylor4 <tom@soonfutures.com>
2025-06-26 19:39:45 +1000

    Update supabase-proxy.js

--- api/supabase-proxy.js
+++ api/supabase-proxy.js
@@ -6,8 +6,8 @@ export default async function handler(req, res) {
 
   const response = await fetch(url.toString(), {
     headers: {
-      apikey: process.env.SUPABASE_KEY,
-      Authorization: `Bearer ${process.env.SUPABASE_KEY}`
+      apikey: process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxY25tamt4dHFva212YWRzZW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjU3MTUsImV4cCI6MjA2NjQwMTcxNX0.gL3XTcUw24-J6Xwk1tpwErUARAuiwkwS-VCy2dbZU8k,
+      Authorization: `Bearer ${process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxY25tamt4dHFva212YWRzZW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjU3MTUsImV4cCI6MjA2NjQwMTcxNX0.gL3XTcUw24-J6Xwk1tpwErUARAuiwkwS-VCy2dbZU8k}`
     }
   });
 
